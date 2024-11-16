75db5cb69036cdcb98be3121cadd5d7ae01b76bc
jusa11 <a.juiceit@gmail.com>
2024-11-16 22:35:13 +0500

    edit generateRandomQoutes

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 'use strict';
 import qoutes from './src/qoutes.js';
-import { hideFavoriteCard, showFavoriteCard, checkfavoriteBtn } from './src/favoritesHandler.js';
+import {
+  hideFavoriteCard,
+  showFavoriteCard,
+  checkfavoriteBtn,
+} from './src/favoritesHandler.js';
+import { generateRandomInt } from './src/utilits.js';
 
 const generateBtn = document.getElementById('generate-btn');
 const qouteAuthor = document.querySelector('.quotes-content-author');
 const qouteText = document.querySelector('.quotes-content-text');
 const favoritesText = document.querySelector('.favorites-qoutes-item');
 const favoritesBtn = document.querySelector('.favorites-btn');
-let qoute;
+let currentQoute;
 
 const generateRandomQoutes = () => {
-  qoute = qoutes[Math.floor(Math.random() * qoutes.length)];
-  const { text, author } = qoute;
+  const randomIndex = generateRandomInt(qoutes.length);
+  currentQoute = qoutes[randomIndex];
+  const { text, author } = currentQoute;
   qouteText.textContent = `"${text}"`;
   qouteAuthor.textContent = author;
-  checkfavoriteBtn(qoute, favoritesBtn);
+  checkfavoriteBtn(currentQoute, favoritesBtn);
 };
 
 const addToFavorites = () =>
-  qoute.isFavorite
-    ? hideFavoriteCard(qoute, favoritesBtn)
-    : showFavoriteCard(qoute, favoritesBtn, favoritesText);
+  currentQoute.isFavorite
+    ? hideFavoriteCard(currentQoute, favoritesBtn)
+    : showFavoriteCard(currentQoute, favoritesBtn, favoritesText);
 
 generateBtn.addEventListener('click', generateRandomQoutes);
 favoritesBtn.addEventListener('click', addToFavorites);
